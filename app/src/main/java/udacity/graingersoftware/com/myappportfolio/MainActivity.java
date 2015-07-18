f5435932e531adbda82435af6e5f7379409d5a81
Michael Whitt <mpwhitt5134@yahoo.com>
2015-07-18 14:59:43 -0500

    Project 0 finished

--- app/src/main/java/udacity/graingersoftware/com/myappportfolio/MainActivity.java
+++ app/src/main/java/udacity/graingersoftware/com/myappportfolio/MainActivity.java
@@ -1,19 +1,93 @@
 package udacity.graingersoftware.com.myappportfolio;
 
-import android.support.v7.app.ActionBarActivity;
+import android.content.Context;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 
 public class MainActivity extends ActionBarActivity
 {
+
+    private Button mButtonOne;
+    private Button mButtonTwo;
+    private Button mButtonThree;
+    private Button mButtonFour;
+    private Button mButtonFive;
+    private Button mButtonSix;
+    private Context mContext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mContext = this;
+
+        mButtonOne = (Button)findViewById(R.id.buttonOne);
+        mButtonOne.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(final View v)
+            {
+                Toast.makeText(mContext, getResources().getString(R.string.button_one_text), Toast.LENGTH_LONG).show();
+            }
+        });
+
+        mButtonTwo = (Button)findViewById(R.id.buttonTwo);
+        mButtonTwo.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(final View v)
+            {
+                Toast.makeText(mContext, getResources().getString(R.string.button_two_text), Toast.LENGTH_LONG).show();
+            }
+        });
+
+        mButtonThree = (Button)findViewById(R.id.buttonThree);
+        mButtonThree.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(final View v)
+            {
+                Toast.makeText(mContext, getResources().getString(R.string.button_three_text), Toast.LENGTH_LONG).show();
+            }
+        });
+
+        mButtonFour = (Button)findViewById(R.id.buttonFour);
+        mButtonFour.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(final View v)
+            {
+                Toast.makeText(mContext, getResources().getString(R.string.button_four_text), Toast.LENGTH_LONG).show();
+            }
+        });
+
+        mButtonFive = (Button)findViewById(R.id.buttonFive);
+        mButtonFive.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(final View v)
+            {
+                Toast.makeText(mContext, getResources().getString(R.string.button_five_text), Toast.LENGTH_LONG).show();
+            }
+        });
+
+        mButtonSix = (Button)findViewById(R.id.buttonSix);
+        mButtonSix.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(final View v)
+            {
+                Toast.makeText(mContext, getResources().getString(R.string.button_six_text), Toast.LENGTH_LONG).show();
+            }
+        });
     }
 
     @Override
